b1feeedf2debeab91883e922b52bd6960b05c443
ismael094 <ismaelpitufo@gmail.com>
2020-01-18 12:15:52 +0000

    First test: red test

---
+++ src/test/java/PigIt_.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PigIt_ {
+    @Test
+    public void order_an_empty_string_should_return_empty() {
+        assertThat(pigIt("")).isEqualTo("");
+    }
+
+    private String pigIt(String pharse) {
+        return null;
+    }
+}
