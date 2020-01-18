5b3deffbcd504026b34636d5908af01fc2b97445
ismael094 <ismaelpitufo@gmail.com>
2020-01-18 12:17:33 +0000

    Second test: red test

--- src/test/java/PigIt_.java
+++ src/test/java/PigIt_.java
@@ -4,8 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PigIt_ {
     @Test
-    public void order_an_empty_string_should_return_empty() {
+    public void pigit_an_empty_string_should_return_empty() {
         assertThat(pigIt("")).isEqualTo("");
+    }
+
+    @Test
+    public void pigit_Hola_string_should_return_olaHay() {
+        assertThat(pigIt("Hola")).isEqualTo("olaHay");
     }
 
     private String pigIt(String pharse) {
